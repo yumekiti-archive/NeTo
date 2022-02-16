609569f00f47e3387906056007bc531d99562e0a
yumekiti <yumekiti120440@gmail.com>
2022-02-16 13:13:10 +0900

    same

--- nest/src/app.controller.ts
+++ nest/src/app.controller.ts
@@ -5,7 +5,7 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get("/api")
+  @Get("api")
   getHello(): string {
     return this.appService.getHello();
   }
