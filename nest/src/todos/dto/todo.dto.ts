e19206d56f875beb78e6170e912fd53760a2a14a
yumekiti <yumekiti120440@gmail.com>
2022-03-15 21:59:34 +0900

    `same`

--- nest/src/todos/dto/todo.dto.ts
+++ nest/src/todos/dto/todo.dto.ts
@@ -9,4 +9,24 @@ export class NewTodo {
 
   @Field()
   body: string;
+
+  @Field()
+  @IsNotEmpty()
+  isStatus: boolean;
 }
+
+// # Write your query or mutation here
+// mutation {
+//   addNewTodo(newTodoData: {
+//     title: "牛乳を買ってくる",
+//     body: "",
+//     isStatus: false,
+//   }) {
+//     id
+//     title
+//     body
+//     isStatus
+//     createdAt
+//     updatedAt
+//   }
+// }
